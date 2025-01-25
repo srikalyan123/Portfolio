a0bbc49fabccfb183471e595594b026e92570461
Srikalyan123 <srikalyanmarri999@gmail.com>
2025-01-25 14:39:52 -0600

    Navigation Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Navigation from "./components/Navigation";
+import Navigation from "./Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
